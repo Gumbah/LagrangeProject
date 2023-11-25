3d014f58643bd88d10014653dd16b5a685aeca32
jakb-t <jakubftucker@gmail.com>
2023-11-25 20:07:07 +0000

    Proved some lemmas about my extended euclidean alg

--- LagrangeProject/NumTheory.lean
+++ LagrangeProject/NumTheory.lean
@@ -52,7 +52,7 @@ def div_lists (x y : ℕ) : (List ℤ × List ℤ) := Id.run do
 --until we reach r_0 and r_1 (which are x, y) giving us an
 --equation of the form r_n = ax+by for some a,b ∈ ℤ.
 
-def bezout_coeffs (x y : ℕ) : (ℤ × ℤ) := Id.run do
+def bezout_coeffs_old (x y : ℕ) : (ℤ × ℤ) := Id.run do
   let (D : List ℤ) := ((div_lists x y).1).reverse
   let mut (a : ℤ) := (1 : ℤ)
   let mut (b : ℤ) := -(D[0]!)
@@ -63,7 +63,7 @@ def bezout_coeffs (x y : ℕ) : (ℤ × ℤ) := Id.run do
     a := c
   return (a, b)
 
-#eval bezout_coeffs 2023 70
+#eval bezout_coeffs_old 2023 70
 
 --Now it remains to prove Bézout's lemma, given this explicit
 --construction for the coefficients. I aim to do this by
@@ -78,5 +78,108 @@ def bezout_coeffs (x y : ℕ) : (ℤ × ℤ) := Id.run do
 --proving this lemma. For now I will `sorry` it out and move
 --on.
 
-theorem bezout : ∀ {x y : ℕ}, ∃ (a b : ℤ), a*x+b*y=gcd x y := by
+--24/11/23
+
+--I have now learned how to define such an algorithm recursively.
+--with the help of fellow MA4N1 student Gareth Ma, I learned the syntax for
+--such a recursive definition, and translated the algorithm found
+--at `https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Recursive_algorithm_2`
+--into LEAN, I used the Python recursive algorithm as it is the language
+--I am most familiar with. With this new definition I hope I will be able
+--to finally prove Bézout's lemma using induction.
+
+def gcd_bezout : ℕ → ℕ → (ℕ × ℤ × ℤ)
+| 0, y => (y, 0, 1)
+| (x + 1), y =>
+  /- for well-foundedness -/
+  have := Nat.mod_lt y (Nat.succ_pos x)
+  let (g, a, b) := gcd_bezout (y%(x+1)) (x+1)
+  (g, b - y/(x+1) * a, a)
+
+--checking it works, and agrees with the old algorithm
+#eval bezout_coeffs_old 2023 70
+#eval gcd_bezout 2023 70
+#eval gcd_bezout (2023%70) 2023
+#eval bezout_coeffs_old 20 9
+#eval (gcd_bezout 20 9).2.1
+
+--25/11/23
+
+--I want to prove Bézout's lemma, but first we need some more definitions and lemmas. I am
+--proving many basic facts about my `gcd_bezout` function in order to make the final proof of
+--Bézout's lemma as simple as possible.
+
+--I wanted to lead up to proving that the first term of the output of the `gcd_bezout` function,
+--labelled `my_gcd` is equal to the gcd already in mathlib, but this proved difficult, as
+--I do not understand how gcd is defined within mathlib, I may instead use my own definition of
+--gcd from the `gcd_bezout` algorithm throughout the rest of the project, or prove it is equivalent
+--to a more concise and useful definition I can make to save unfolding `gcd_bezout` every time.
+
+--I have tried looking at the proof of Bézout's lemma that is already in mathlib (`https://github.com/leanprover-community/mathlib/blob/65a1391a0106c9204fe45bc73a039f056558cb83/src/data/int/gcd.lean#L107`)
+--for inspiration on how to prove it for myself, but the proofs there are very concise and it
+--is difficult to understand what each part is actually doing, I don't think that this gives much
+--insight into my own proving of this theorem.
+
+def bez_a (x y : ℕ): ℤ := (gcd_bezout x y).2.1
+def bez_b (x y : ℕ): ℤ := (gcd_bezout x y).2.2
+def my_gcd (x y : ℕ) : ℕ := (gcd_bezout x y).1
+
+@[simp] lemma gcd_bez_expand (x y : ℕ) : gcd_bezout x y = (my_gcd x y, bez_a x y, bez_b x y) := by
+  unfold my_gcd bez_a bez_b
+  rfl
+
+@[simp] lemma gcd_bez_zero_left {y : ℕ} : gcd_bezout Nat.zero y = (y, 0, 1) := by
+  unfold gcd_bezout
+  rfl
+
+@[simp] lemma bez_a_zero_left {y : ℕ} : bez_a Nat.zero y = 0 := by
+  unfold bez_a
+  simp? says simp only [Nat.zero_eq, gcd_bez_zero_left]
+
+@[simp] lemma bez_b_zero_left {y : ℕ} : bez_b Nat.zero y = 1 := by
+  unfold bez_b
+  simp? says simp only [Nat.zero_eq, gcd_bez_zero_left]
+
+@[simp] lemma my_gcd_zero_left {y : ℕ} : my_gcd Nat.zero y = y := by
+  unfold my_gcd
+  simp? says simp only [Nat.zero_eq, gcd_bez_zero_left]
+
+@[simp] lemma bez_a_zero_right {x : ℕ} (h : x ≠ 0): bez_a x Nat.zero = 1 := by
+  unfold bez_a gcd_bezout
+  induction x with
+  | zero => exact absurd rfl h
+  | succ x => simp? says simp only [Nat.zero_eq, Nat.zero_mod, gcd_bez_zero_left, CharP.cast_eq_zero,
+    EuclideanDomain.zero_div, mul_zero, sub_zero]
+
+@[simp] lemma bez_b_zero_right {x : ℕ} (h : x ≠ 0): bez_b x Nat.zero = 0 := by
+  unfold bez_b gcd_bezout
+  induction x with
+  | zero => exact absurd rfl h
+  | succ x => simp? says simp only [Nat.zero_eq, Nat.zero_mod, gcd_bez_zero_left]
+
+@[simp] lemma my_gcd_zero_right {x : ℕ} (h : x ≠ 0): my_gcd x Nat.zero = x := by
+  unfold my_gcd gcd_bezout
+  induction x with
+  | zero => exact absurd rfl h
+  | succ x => simp? says simp only [Nat.zero_eq, Nat.zero_mod, gcd_bez_zero_left]
+
+@[simp] lemma gcd_bez_zero_right {x : ℕ} (h : x ≠ 0): gcd_bezout x Nat.zero = (x, 1, 0) := by
+  rw[gcd_bez_expand x Nat.zero]
+  induction x with
+  | zero => exact absurd rfl h
+  | succ => simp? says simp only [Nat.zero_eq, ne_eq, Nat.succ_ne_zero, not_false_eq_true, my_gcd_zero_right,
+    bez_a_zero_right, bez_b_zero_right]
+
+@[simp] lemma my_gcd_self {x : ℕ} : my_gcd x x = x := by
+  induction x with
+  | zero => apply my_gcd_zero_left
+  | succ =>
+    unfold my_gcd gcd_bezout
+    simp only [Nat.mod_self, gcd_bez_expand, my_gcd_zero_left, bez_a_zero_left, bez_b_zero_left]
+
+@[simp] theorem my_gcd_is_gcd {x y : ℕ} : my_gcd x y = Nat.gcd x y := by
+  unfold Nat.gcd x y
+
+
+theorem bezout {x y : ℕ} : (bez_a x y)*x+(bez_b x y)*y=(gcd x y) := by
   sorry
